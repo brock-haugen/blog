f5cf4d05451b709784e030df3b65759b556c9e81
Brock Haugen <brock@bridgetowncollective.com>
2021-05-22 13:30:17 -0700

    adding oauth-in-nodejs.md

--- src/posts/index.js
+++ src/posts/index.js
@@ -1,6 +1,7 @@
 const posts = Object.entries({
   "2021-05-18.testing": require("./2021-05-18.testing.md"),
   "2021-05-19.on-blogging": require("./2021-05-19.on-blogging.md"),
+  "2021-05-21.oauth-in-nodejs": require("./2021-05-21.oauth-in-nodejs.md"),
 }).map(([fp, { data, content }]) => {
   const [date, slug] = fp.split(".");
 
